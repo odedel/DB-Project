110570747dbe1d1f9bc1d667a0c71d8715952213
Oded <odedelbaz@gmail.com>
2015-05-08 17:38:42 +0300

    Added db package

---
+++ src/db/DBConnection.java
@@ -0,0 +1,56 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    private Connection conn;
+
+    /** Opens new connection to the db and initialize conn with it. Returns true if the connection is open */
+    public boolean connect() {
+
+        // loading the driver
+        try {
+            Class.forName(JDBC_DRIVER);
+        } catch (ClassNotFoundException e) {
+            System.out.println("Unable to load the MySQL JDBC driver..");
+            return false;
+        }
+        System.out.println("Driver loaded successfully");
+
+        // creating the connection
+        System.out.print("Trying to connect... ");
+        try {
+            conn = DriverManager.getConnection(String.format(CONNECTION_STRING, DEFAULT_HOST, DEFAULT_SCHEMA), DEFAULT_USER, DEFAULT_PASSWORD);
+        } catch (SQLException e) {
+            System.out.println("Unable to connect - " + e.getMessage());
+            conn = null;
+            return false;
+        }
+        System.out.println("Connected!");
+        return true;
+    }
+
+    /**
+     * close the connection
+     */
+    public void close() {
+        try {
+            conn.close();
+            System.out.println("Connection closed!");
+        } catch (SQLException e) {
+            System.out.println("Unable to close the connection - "
+                    + e.getMessage());
+        }
+
+    }
+
+    private static String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    private static String CONNECTION_STRING = "jdbc:mysql://%s/%s";
+    private static String DEFAULT_HOST = "localhost:3306";
+    private static String DEFAULT_SCHEMA = "sakila";
+    private static String DEFAULT_USER = "root";
+    private static String DEFAULT_PASSWORD = "root";
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,6 @@
 package main;
 
-import questions.Question;
-import questions.QuestionGenerator;
+import db.DBConnection;
 
 /**
  * Created by Oded on 08/05/2015.
@@ -9,11 +8,15 @@ import questions.QuestionGenerator;
 public class Main {
 
     public static void main(String[] args) {
-        Question q = new Question("Hello?", "Hey", "a", "b", "c");
+//        Question q = new Question("Hello?", "Hey", "a", "b", "c");
+//
+//        System.out.println(q.checkAnswer("Hey"));
+//        System.out.println(q.checkAnswer("a"));
+//
+//        System.out.println(QuestionGenerator.questionGenerator());
 
-        System.out.println(q.checkAnswer("Hey"));
-        System.out.println(q.checkAnswer("a"));
-
-        System.out.println(QuestionGenerator.questionGenerator());
+        DBConnection connector = new DBConnection();
+        connector.connect();
+        connector.close();
     }
 }
