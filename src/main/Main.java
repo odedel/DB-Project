9f499722e7e61a90a40039f9eb58373da8290f67
Oded <odedelbaz@gmail.com>
2015-05-08 17:04:23 +0300

    Added stupid main

---
+++ src/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+
+/**
+ * Created by Oded on 08/05/2015.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Question q = new Question("Hello?", "Hey", "a", "b", "c");
+
+        System.out.println(q.checkAnswer("Hey"));
+        System.out.println(q.checkAnswer("a"));
+    }
+}
