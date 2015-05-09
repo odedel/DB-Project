016b03a87ac0ac5215127e074330c127b5a1e030
Tomer Greenwald <tomerg@gmail.com>
2015-05-09 09:27:10 +0300

    Moved folder

--- src/main/sample.java
+++ src/main/sample.java
@@ -3,7 +3,10 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.lang.System;
-import java.util.*;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
 
 
 class Row {
