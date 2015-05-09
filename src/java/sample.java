1a654be0aa7bede21636de2797c1dd93752da25e
Tomer Greenwald <tomerg@gmail.com>
2015-05-09 09:02:52 +0300

    More functions

--- src/java/sample.java
+++ src/java/sample.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.lang.System;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.LinkedList;
 
 
@@ -27,36 +28,96 @@ class Row {
         this.superEntity = split[3];
     }
 }
+
+class Tuple {
+    String first;
+    String second;
+
+    public Tuple(String first, String second) {
+        this.first = first;
+        this.second = second;
+    }
+}
+
+enum Attribute {
+    ID,
+    FIRST_ENTITY,
+    RELATION_TYPE,
+    SECOND_ENTITY
+}
+
 class Sample {
 
     static String COUNTRY_TYPE = "<wikicat_Countries>";
+    static String PREF_LABEL = "skos:prefLabel";
 
     enum Type {
         ID, ENTITY, RELATION_TYPE, SUPER_ENTITY
     }
 
-    public static void main(String args[]) throws Exception{
-        FileInputStream fis = new FileInputStream("c:\\Users\\Tomer\\Documents\\DB-tau\\DB-Project\\yago\\yagoTypes.tsv");
-        BufferedReader reader = new BufferedReader(new InputStreamReader(fis));
-        String line = reader.readLine();
-
-        Collection<String> countries = collectEntitiesByType(reader, COUNTRY_TYPE);
+    public static void main(String args[]) throws Exception {
+        Collection<String> countries = collectCountries();
         System.out.println(countries);
+
 
     }
 
-    private static Collection<String> collectEntitiesByType(BufferedReader reader, String entityType) throws IOException {
+    private static Collection<String> collectCountries() throws IOException {
+        Collection<String> countries = getCountryIDs();
+        //Collection<String> countries = Collections.singletonList("<id_1lqi1ft_88c_1ihryd7>");
+        System.out.println(1);
+        Collection<String> countryNames = getCountryNames(countries);
+        System.out.println(2);
+        int i = 2;
+        return countryNames;
+    }
+
+    private static Collection<String> getCountryNames(Collection<String> countries) throws IOException {
+        FileInputStream fis = new FileInputStream("c:\\Users\\Tomer\\Documents\\DB-tau\\DB-Project\\yago\\yagoLabels.tsv");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(fis));
+        return reduceEntitiesByAttributeFromCollection(reader, PREF_LABEL, Attribute.RELATION_TYPE, countries);
+    }
+
+    private static Collection<String> getCountryIDs() throws IOException {
+        FileInputStream fis = new FileInputStream("c:\\Users\\Tomer\\Documents\\DB-tau\\DB-Project\\yago\\yagoTypes.tsv");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(fis));
+        return collectEntitiesByAttribute(reader, COUNTRY_TYPE, Attribute.SECOND_ENTITY, Attribute.FIRST_ENTITY);
+    }
+
+    private static Collection<String> collectEntitiesByAttribute(BufferedReader reader, String entityType, Attribute attribute, Attribute collectedAttribute) throws IOException {
         String line;
         Collection<String> collection = new LinkedList<>();
         line = reader.readLine();
-        while(line != null){
+        while (line != null) {
             String[] split = line.split("\t");
-            Row row = new Row(split);
-            if(row.superEntity.equals(entityType)) {
-                collection.add(row.entity);
+            if (split[attribute.ordinal()].equals(entityType)) {
+                collection.add(split[collectedAttribute.ordinal()]);
             }
             line = reader.readLine();
         }
         return collection;
     }
+
+    private static Collection<String> reduceEntitiesByAttributeFromCollection(BufferedReader reader, String entityType, Attribute attribute, Collection<String> ids) throws IOException {
+        String line;
+        Collection<String> collection = new LinkedList<>();
+        line = reader.readLine();
+        Collection<String> iids = new LinkedList<>();
+        while (line != null) {
+            String[] split = line.split("\t");
+            if (split[attribute.ordinal()].equals(entityType)) {
+                if (ids.contains(split[1])) {
+                    collection.add(split[Attribute.ID.ordinal()]);
+                }
+            }
+            line = reader.readLine();
+        }
+        return collection;
+    }
+
+    private static Collection<String> collectEntitiesByRelation(BufferedReader reader, String relationType) throws IOException {
+        return collectEntitiesByAttribute(reader, PREF_LABEL, Attribute.RELATION_TYPE, Attribute.FIRST_ENTITY);
+    }
+
+
 }
