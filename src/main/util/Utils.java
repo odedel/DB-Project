722849bd6a626efb9fde4035d31bebf0963a73ed
Tomer Greenwald <tomerg@gmail.com>
2015-05-09 19:03:45 +0300

    Allowing varargs

--- src/main/util/Utils.java
+++ src/main/util/Utils.java
@@ -6,14 +6,16 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Utils {
-    public static void reduceEntitiesByAttributeFromCollectionWithMatcher(String filePath, Callback callback) throws IOException {
+    public static void reduceEntitiesByAttributeFromCollectionWithMatcher(String filePath, Callback... callbacks) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(filePath)));
         String line = reader.readLine();
         while (line != null) {
             String[] split = line.split("\t");
             Row row = new Row(split);
-            if (callback.map(row)) {
-                callback.reduce(row);
+            for (Callback callback : callbacks) {
+                if (callback.map(row)) {
+                    callback.reduce(row);
+                }
             }
             line = reader.readLine();
         }
