8afd190fe89d40e259fb9d80bfe1b80d964e861a
Tomer Greenwald <tomerg@gmail.com>
2015-05-15 10:48:42 +0300

    Reformatted

--- src/main/data/Place.java
+++ src/main/data/Place.java
@@ -8,23 +8,23 @@ import java.util.List;
 
 public class Place {
 
-    public String name;
-    public List<Row> facts = new LinkedList<>();
-    public LocalDate creationDate;
+    public String       name;
+    public List<Row>    facts = new LinkedList<>();
+    public LocalDate    creationDate;
     public List<String> places = new LinkedList<>();
-    public float export;            // Dollar
-    public float expenses;          // Dollar
-    public float latitude;          // Degress
-    public float longitude;         // Degress
-    public float economicGrowth;    // %
-    public float poverty;           // %
-    public long population;
-    public float unemployment;      // %
-    public float revenue;           // Dollar
-    public float gini;
-    public float _import;           // Dollar
-    public float gdp;               // Dollar
-    public float inflation;         // %
-    public float populationDensity; // 1/km^2
-    public String tld;
+    public float        export;                         // Dollar
+    public float        expenses;                       // Dollar
+    public float        latitude;                       // Degress
+    public float        longitude;                      // Degress
+    public float        economicGrowth;                 // %
+    public float        poverty;                        // %
+    public long         population;
+    public float        unemployment;                   // %
+    public float        revenue;                        // Dollar
+    public float        gini;
+    public float        _import;                        // Dollar
+    public float        gdp;                            // Dollar
+    public float        inflation;                      // %
+    public float        populationDensity;              // 1/km^2
+    public String       tld;
 }
