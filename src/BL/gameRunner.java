ce8618406cc7fc10230f524445cbf37e2687d2fe
yaron libman <libmanya@gmail.com>
2015-06-15 22:36:01 +0300

    Most Questions inside, not tested because DB is not valid on tau Most function for UI

--- src/BL/gameRunner.java
+++ src/BL/gameRunner.java
@@ -1,20 +1,27 @@
 package BL;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import collect_data.DataCollector;
+import utils.DBUser;
+import utils.DataNotFoundException;
+import utils.EntityNotFound;
 import utils.IDName;
 import dao.DAO;
 import dao.DAOException;
+import main.*;
 
 public class gameRunner {
 	  
 	public static void main(String[] args) throws Exception {
-		new gameRunner();
+		gameRunner gm = new gameRunner();
+		gm.getCurrentQuestion();
 	  }
 	private User playerOne = null;
 	private User playerTwo = null;
-	private Question currQuestion;
+	private Question currQuestion = null;
 	private QuestionFactory qFactory;
 	private User currentPlayer;
 	private int nPlayerOneWorngAnswers;
@@ -29,35 +36,50 @@ public class gameRunner {
 		this.nPlayerTwoWorngAnswers = 0;
 		this.qFactory = new QuestionFactory();
 		this.access = new DAO();
+		this.access.connect(DBUser.MODIFIER);
 		this.allCountries = this.access.getAllCountries();
 	}
 	
-	public Collection<IDName> getAllCounter() throws DAOException
+	//Use this function when exit
+	public void  CloseGameRunner()
+	{
+		this.access.disconnect();
+	}
+	
+	//Return all countries, every node is ID and Name
+	public Collection<IDName> getAllCountries() throws DAOException
 	{
 		return(this.allCountries);
 	}
 	
-	public String getCurrentQuestion()
+	//This function create Question if needed and return question string
+	public String getCurrentQuestion() throws DAOException, DataNotFoundException, EntityNotFound
 	{
+		if(this.currQuestion == null)
+		{
+			this. currQuestion = this.qFactory.createNewQuestion(null);
+		}
+		
 		return this.currQuestion.getQuestion();
 	}
 
+	//This function return shuffle answers
 	public ArrayList<String> getAnswers()
 	{
 		return(this.currQuestion.getAnswers());
 	}
 	
 	//Return false if user login failed or two player ar already logged
-	public boolean logInUser(String strUserName, String strPassword)
+	public boolean logInUser(String strUserName, String strPassword) throws DAOException, DataNotFoundException
 	{
 		if(this.playerOne == null)
 		{
-			this.playerOne = new User(strUserName, strPassword);
+			this.playerOne = new User(strUserName, strPassword, this.access);
 			return(this.playerOne.isLoggedIn());
 		}
 		else if(this.playerTwo == null)
 		{
-			this.playerTwo = new User(strUserName, strPassword);
+			this.playerTwo = new User(strUserName, strPassword, this.access);
 			return(this.playerTwo.isLoggedIn());
 		}
 		
@@ -67,7 +89,7 @@ public class gameRunner {
 	//Return false if user Register failed or Not
 	public boolean registerUser(String strUserName, String strPassword) throws DAOException
 	{
-		return(User.registerUser(strUserName, strPassword));
+		return(User.registerUser(strUserName, strPassword,this.access));
 	}
 	
 	//This function continue the game (switch player if need and get new Ques)
@@ -113,6 +135,7 @@ public class gameRunner {
 		if(this.currQuestion.checkAnswer(strAns))
 		{
 			this.currentPlayer.addRightAnswerScore();
+			this.currentPlayer.setUserAnsweredCorrectly();
 			isRight = true;
 		}
 		else
@@ -125,7 +148,7 @@ public class gameRunner {
 	}
 	
 	//Add wrong answer to current user
-	private void addWrongAnswer()
+	private void addWrongAnswer() throws DAOException, EntityNotFound
 	{
 		if(this.currentPlayer == this.playerOne)
 		{
@@ -135,6 +158,8 @@ public class gameRunner {
 		{
 			this.nPlayerTwoWorngAnswers++;
 		}
+		
+		this.currentPlayer.setUserAnsweredWrong();
 	}
 	
 	//check if game finished
@@ -145,14 +170,71 @@ public class gameRunner {
 			return (true);
 		}
 		
-		
 		return false;
 	}
 	
-	//This function init game to non wrong answers
-	public void initGame()
+	//This function init game to non wrong answers and player one to start
+	public void StartGame() throws DAOException, EntityNotFound
 	{
 		this.nPlayerOneWorngAnswers = 0;
 		this.nPlayerTwoWorngAnswers = 0;
+		
+		if(this.playerOne != null)
+		{
+			this.playerOne.initPlayerScore();
+			this.currentPlayer = this.playerOne;
+			this.playerOne.setUserStartedNewGame();
+		}
+		
+		if(this.playerTwo != null)
+		{
+			this.playerTwo.initPlayerScore();
+			this.playerTwo.setUserStartedNewGame();
+		}
+	}
+	
+	//Update DataFrom Yago
+	public void updateDataFromSource() throws DAOException, IOException
+	{
+		this.access.deleteDB();
+		DataCollector dataCollector = new DataCollector();
+		dataCollector.collectData();
+		this.access.uploadDataCollector(dataCollector);
+	}
+	
+	//Count how many players connected 
+	public int getHowPlayerConnected()
+	{
+		int nCount = 0;
+		
+		if(this.playerOne != null)
+		{
+			nCount++;
+		}
+		
+		if(this.playerTwo != null)
+		{
+			nCount++;
+		}
+		
+		return (nCount);
+	}
+	
+	//this function return who has  
+	public int currentWinner()
+	{
+		int nCount = 0;
+		
+		if(this.playerOne != null)
+		{
+			nCount++;
+		}
+		
+		if(this.playerTwo != null)
+		{
+			nCount++;
+		}
+		
+		return (nCount);
 	}
 }
